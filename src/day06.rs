67a407be001209413b124182d56e79c95da30031
Virgile Theuerlacher <vtheuerlacher@yahoo.fr>
2020-12-06 12:10:33 +0100

    day 6: replace HashSet with bitwise operations

--- src/day06.rs
+++ src/day06.rs
@@ -1,8 +1,18 @@
 use crate::day::Day;
-use std::collections::HashSet;
 
 pub struct Day06 {
-    groups: Vec<Vec<HashSet<char>>>,
+    groups: Vec<Vec<u32>>,
+}
+
+impl Day06 {
+    fn sum_groups_with<F: Fn(u32, &u32) -> u32>(&self, neutral: u32, folder: F) -> Box<u32> {
+        Box::new(
+            self.groups
+                .iter()
+                .map(|group| group.iter().fold(neutral, &folder).count_ones())
+                .sum::<u32>(),
+        )
+    }
 }
 
 impl Day for Day06 {
@@ -10,38 +20,25 @@ impl Day for Day06 {
         Day06 {
             groups: input
                 .split("\n\n")
-                .map(|group| group.lines().map(|l| l.chars().collect()).collect())
+                .map(|group| {
+                    group
+                        .lines()
+                        .map(|l| {
+                            l.chars()
+                                .map(|c| c as u8 - 'a' as u8)
+                                .fold(0, |a, c| a | (1 << c))
+                        })
+                        .collect()
+                })
                 .collect(),
         }
     }
 
     fn part_1(&self) -> Box<dyn ToString + '_> {
-        Box::new(
-            self.groups
-                .iter()
-                .map(|group| {
-                    group
-                        .iter()
-                        .fold(HashSet::new(), |a, b| a.union(b).map(|&c| c).collect())
-                        .len()
-                })
-                .sum::<usize>(),
-        )
+        self.sum_groups_with(0, |a, b| a | b)
     }
 
     fn part_2(&self) -> Box<dyn ToString> {
-        Box::new(
-            self.groups
-                .iter()
-                .map(|group| {
-                    group
-                        .iter()
-                        .fold(('a'..='z').collect::<HashSet<_>>(), |a, b| {
-                            a.intersection(b).map(|&c| c).collect()
-                        })
-                        .len()
-                })
-                .sum::<usize>(),
-        )
+        self.sum_groups_with((0..26).fold(0, |a, n| a | (1 << n)), |a, b| a & b)
     }
 }
