2953909f1e8825a3f72a297910af5e0386ddbe42
Virgile <virgile.theuerlacher@worldline.com>
2020-12-17 15:47:21 +0100

    Refactor main and macro to avoid match

--- src/macros.rs
+++ src/macros.rs
@@ -1,35 +1,25 @@
 extern crate proc_macro;
 
-use itertools::Itertools;
 use proc_macro::TokenStream;
 
 #[proc_macro]
-pub fn run_day(token_stream: TokenStream) -> TokenStream {
+pub fn days_vec(token_stream: TokenStream) -> TokenStream {
     let tokens = token_stream
         .into_iter()
         .map(|t| t.to_string())
         .collect::<Vec<_>>();
-    assert_eq!(
-        tokens.len(),
-        2,
-        "run_day requires a day count and a day number"
-    );
+    assert_eq!(tokens.len(), 1, "run_day requires a day count");
     let day_count = tokens
         .get(0)
         .unwrap()
         .parse::<u8>()
         .expect("day count must be a u8 litteral");
-    let day_number = tokens.get(1).unwrap();
 
     format!(
-        "match {0} {{{1}, _ => unreachable!(\"day {{}} not found\", {0})}}",
-        day_number,
+        "vec![{}]",
         (1..=day_count)
-            .map(|n| format!(
-                "{0} => {{crate::day{0:02}::Day{0:02}::run({0}, &std::fs::read_to_string(\"inputs/{0:02}.txt\").unwrap())}}",
-                n
-            ))
-            .join(","),
+            .map(|n| format!("|i| {{crate::day{0:02}::Day{0:02}::run({0}, i)}},", n))
+            .collect::<String>(),
     )
     .parse()
     .unwrap()
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-extern crate colored;
 extern crate macros;
 
 use std::env;
-use std::fs::read_dir;
+use std::fs::{read_dir, read_to_string};
 
 use crate::day::Day;
 use crate::util::format_duration;
 use colored::*;
-use macros::run_day;
+use macros::days_vec;
 
 mod day;
 mod day01;
@@ -29,11 +28,15 @@ mod day16;
 mod day17;
 mod util;
 
+fn run_day(days: &[fn(&str) -> u128], n: u8) -> u128 {
+    assert!(n <= days.len() as u8, "day {} not found", n);
+    days[n as usize - 1](&read_to_string(format!("inputs/{:02}.txt", n)).unwrap())
+}
+
 fn day_from_input() -> Option<u8> {
     read_dir("inputs")
         .ok()?
-        .filter_map(|e| e.ok())
-        .filter_map(|e| e.file_name().into_string().ok())
+        .filter_map(|e| e.ok()?.file_name().into_string().ok())
         .filter_map(|f| f[..f.find('.')?].parse().ok())
         .max()
 }
@@ -41,21 +44,28 @@ fn day_from_input() -> Option<u8> {
 fn main() {
     let arg = env::args().skip(1).next();
 
+    let days: Vec<for<'r> fn(&'r str) -> u128> = days_vec!(17);
+
     if arg == Some(String::from("-a")) {
         println!(
             "\n{}",
             &format!(
                 "Total run time: {}",
-                format_duration((1..=17).map(|n| run_day!(17 n)).sum::<u128>())
+                format_duration(
+                    (1..=days.len() as u8)
+                        .map(|n| run_day(&days, n))
+                        .sum::<u128>()
+                )
             )
             .bold()
             .cyan()
         );
     } else {
-        let day_number = arg
-            .map(|a| a.parse::<u8>().expect("Could not read day number"))
-            .or_else(day_from_input)
-            .expect("No input file found");
-        run_day!(17 day_number);
+        run_day(
+            &days,
+            arg.map(|n| n.parse().expect("Could not read day number"))
+                .or_else(day_from_input)
+                .expect("No input file found"),
+        );
     }
 }
